587f17283bedb24c67aa327d141f64424e716bc1
= <=>
2021-02-01 18:15:37 -0800

    MIME Type Fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -135,3 +135,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
